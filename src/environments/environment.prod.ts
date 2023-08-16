1a8d61d5772571e1813d3bae201d7081cd67b00a
Manish Yadav <m.yadav7208@gmail.com>
2023-08-16 17:49:25 +0530

    changed path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
+const BASE_URL = 'http://clpcrm.com/thread-craft'
 
 export const environment = {
-  production: true
+  production: true,
+  authServiceURL: BASE_URL + 'auth/',
+  productServiceURL: BASE_URL + 'product/',
+  invoiceServiceURL: BASE_URL + 'invoice/',
+  clientServiceURL: BASE_URL + 'client/',
+  companyURL: BASE_URL + 'company/',
+  vendorURL: BASE_URL + 'vendor/'
 };
